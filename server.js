a31c652fe312554f90769c834c5adaf3509ad276
dzsoldado <klemidz@gmail.com>
2022-12-16 01:19:17 +0100

    added cors support

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const redirector = require('./routes/redirector')
 const api = require('./routes/api')
@@ -10,6 +11,7 @@ const app = express();
 
 // Middlewares
 app.use(bodyParser.json())
+app.use(cors())
 
 // Routes
 app.use('/', redirector)
